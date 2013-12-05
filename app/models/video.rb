eb7a1a3361d38deb6bed30efc2d02a37ccdf3e92
amhayslip <amhayslip@gmail.com>
2013-12-05 15:21:21 -0600

    passed all tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,22 @@ class Video < ActiveRecord::Base
   validates :user_id, :title, :youtube_id, presence: true
 
 
+def avg_rating
+  total = 0.0
+  self.ratings.to_a.each do |rating|
+    total += rating.value
+  end
+  if self.ratings.count == 0
+    return 0
+  else
+    return total / self.ratings.count
+  end
+end
+
+
+
+
+
+
+
 end
